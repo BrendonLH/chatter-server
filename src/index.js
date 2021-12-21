5efe586a715f506acd12d1ce6d0cf40cdaa09223
Brend <brendonhampton1988@gmail.com>
2021-12-20 22:00:23 -0800

    fixing server start on the http create server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = require(http.createServer(app));
+const server = http.createServer(app);
 
 const { Server } = require('socket.io');
 const io = new Server(server)
