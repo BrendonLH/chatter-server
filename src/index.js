d1a79980378f98e87cc01a757bb8accb5d8b059e
Brend <brendonhampton1988@gmail.com>
2021-12-24 00:22:34 -0800

    integrated express with cors and socket io

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
-
-
-const io = require('socket.io')(3000, {
+const io = new Server(server, {
     cors:{
         origin: ['http://localhost:8080'],
     },
+});
+
+app.get('/', (request, response) => {
+    response.send('<h1>Chatter Server Page</h1>')
 })
 
-io.on('connection', socket => {
-    console.log(socket.id);
-})
+io.on("connection", (socket) => {
+  console.log(socket.id);
+});
+
+server.listen(process.env.PORT||3000);
