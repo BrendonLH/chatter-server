d24589dcdd3767c37cb612d75d6411d0f8944128
Brend <brendonhampton1988@gmail.com>
2021-12-24 00:37:03 -0800

    got socket server conncected to client

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-
+const port = process.env.PORT||3000;
 const io = new Server(server, {
     cors:{
         origin: ['http://localhost:8080'],
@@ -15,7 +15,12 @@ app.get('/', (request, response) => {
 })
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`socket ${socket.id} Connected!`);
+  socket.emit('connected', "you are connected!")
 });
 
-server.listen(process.env.PORT||3000);
+
+
+server.listen(port, () => {
+    console.log(`server is up on ${port}`)
+});
